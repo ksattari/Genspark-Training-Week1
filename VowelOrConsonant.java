8711ab9ad83e11b35abe70570023ab25b9739e2b
ksattari <110104064+ksattari@users.noreply.github.com>
2023-01-24 09:33:48 -0800

    Add files via upload

---
+++ VowelOrConsonant.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class VowelOrConsonant {
+
+    static final String VOWELS = "aeiou";
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a character: ");
+        String character = scanner.nextLine();
+        scanner.close();
+
+        //if -1 is returned, the character is not a vowel
+        int index = VOWELS.indexOf(character.toLowerCase());
+        if(index != -1) {
+            System.out.println( character +" is a vowel.");
+        }
+        else
+            System.out.println(character + " is a consonant.");
+
+    }
+}
