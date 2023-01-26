a94c682f9fa7c4dbe472fc5ab6e3d400d3c3905c
ksattari <110104064+ksattari@users.noreply.github.com>
2023-01-26 10:45:26 -0800

    Add files via upload

---
+++ SortAString.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.util.stream.*;
+import java.util.Collections;
+
+/*
+ *  implementation of sort a string
+ *  input: unsorted  String
+ *  output : sorted String of original characters
+ *
+ */
+public class SortAString {
+
+    public static void main(String[] args) {
+
+        String unsorted = "zyxwvutsrqponmlkjihgfedcba";
+        unsorted = unsorted.replaceAll("\\s+","");
+        System.out.println(unsorted);
+        List<Character> sortedList = Arrays.asList(unsorted.chars()
+                .mapToObj(c -> (char) c)
+                .toArray(Character[]::new));
+        Collections.sort(sortedList);
+        String sortedString = sortedList.toString()     //cut the brackets, space and commas out
+                .substring(1, 3 * sortedList.size() - 1)
+                .replaceAll(", ", "");
+        System.out.println(sortedString);
+
+    }
+}
